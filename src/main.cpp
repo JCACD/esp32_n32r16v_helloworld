dacfefea7ac7fbab0c6f81a2db33c64c32c36887
JCACD <Joestar.Chang@accordance.com.tw>
2026-06-22 14:44:25 +0800

    Add basic Arduino sketch for ESP32-S3
    main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup() {
+  // 初始化序列埠
+  Serial.begin(115200);
+  delay(1000);
+}
+
+void loop() {
+  // 每隔一秒在序列埠監控視窗印出 Hello World
+  Serial.println("Hello World from ESP32-S3!");
+  delay(1000);
+}
