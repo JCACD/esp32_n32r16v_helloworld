721d0cd30aea5094e4eb63fba6083a0e9ba37248
JCACD <Joestar.Chang@accordance.com.tw>
2026-06-23 13:16:49 +0800

    Modify hello world output and add logging
    Updated the log message and changed the output text.

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_log.h"
 
 void app_main(void)
 {
-    while (1)
-    {
-        printf("Hello World from ESP32-S3!\n");
+    printf("Hello world!\n");
+    ESP_LOGI("main", "ESP32-S3 N32R16V is running with 32MB Flash & 16MB PSRAM!");
+    
+    while(1) {
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
